8db2871772a54607b90b3f3664f08fb55a7bbf54
aronmakiika <aronmakiika@gmail.com>
2023-10-18 21:44:26 +0300

    chore: add meta information to the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      titleTemplate: 'template',
+      titleTemplate:
+        'Family Tree 365 - Start your family tree today - free! Your first tree is 100% free. Sign-up to begin your genealogy journey today!',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' }
+        { property: 'og:site_name', content: 'Family Tree 365' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Our user-friendly yet powerful platform lets you create your own family tree the quick and easy way. No technical knowledge is required. Start your family tree today - free!'
+        },
+        { property: 'og:image:width', content: '2500' },
+        { property: 'og:image:height', content: '780' },
+        { name: 'twitter:site', content: '@familytree365' },
+        { name: 'twitter:card', content: 'summary_large_image' }
       ],
       link: [],
       script: []
